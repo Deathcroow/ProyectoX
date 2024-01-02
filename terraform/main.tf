0d42f78b711bac6c2bed40276780c11a2bed13be
Ubuntu <ubuntu@ip-172-31-23-227.ec2.internal>
2024-01-02 21:25:10 +0000

    push

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+#esto quiero decir que version de terraform se va a crear
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+#definir y configurar el proveedor
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA2NIW2JQV2YYP4T4Z"
+  secret_key = "QQK190lOdaZ3PzbwjWC0CAG2IB2yigR+tIidy0ix"
+
+}
+
+resource "aws_instance" "example" {
+  ami              = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type    = "t2.micro"
+  tags = {
+    "Name" = "tf" 
+  }
+}
