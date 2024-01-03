c3f2e3493a6cf54a95a05c4d914c5ba833d6c267
Carlos Arias <154254700+Deathcroow@users.noreply.github.com>
2024-01-03 11:50:51 -0500

    Update main2.tf

--- terraform/main2.tf
+++ terraform/main2.tf
@@ -27,6 +27,6 @@ resource "aws_instance" "example" {
   ami              = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type    = var.instance_type
   tags = {
-    "Name" = "tf" 
+    "Name" = "tsdfdff" 
   }
 }
