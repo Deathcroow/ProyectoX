2067fe6a7ca6b28dcd7aea402f36810580d07ecb
Carlos Arias <154254700+Deathcroow@users.noreply.github.com>
2024-01-10 16:12:32 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,10 @@ variable "instance_type" {
   default     = "t2.micro"
 
 }
+
+variable "access_key" {
+  type        = string
+  description = "access_key"
+  default     = "AKIA2NIW2JQV2YYP4T4Z"
+
+}
