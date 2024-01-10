a90e3a58486b6298460e97fdc210e036629d442d
Carlos Arias <154254700+Deathcroow@users.noreply.github.com>
2024-01-10 16:33:57 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,14 +5,14 @@ variable "instance_type" {
 
 }
 
-variable "access_key" {
+variable "AWS_ACCESS_KEY_IDAWS_ACCESS_KEY_ID" {
   type        = string
   description = "access_key"
   default     = "AKIA2NIW2JQV2YYP4T4Z"
 
 }
 
-variable "secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "secret_key"
   default     = "QQK190lOdaZ3PzbwjWC0CAG2IB2yigR+tIidy0ix"
