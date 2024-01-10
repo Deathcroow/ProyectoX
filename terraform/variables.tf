f3532ab161cb61fadd04410e1404cd13d3a01b74
Carlos Arias <154254700+Deathcroow@users.noreply.github.com>
2024-01-10 16:44:26 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,12 +8,12 @@ variable "instance_type" {
 variable "access_key" {
   type        = string
   description = "access_key"
-  default     = "AKIA2NIW2JQV2YYP4T4Z"
+  default     = ""
 
 }
 
 variable "secret_key" {
   type        = string
   description = "secret_key"
-  default     = "QQK190lOdaZ3PzbwjWC0CAG2IB2yigR+tIidy0ix"
+  default     = ""
 }
